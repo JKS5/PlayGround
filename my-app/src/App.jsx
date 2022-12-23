04d80c63c978937d2aadeeac121eebdab9003a2a
JKS5 <jokekhanson@gmail.com>
2022-12-23 20:42:04 +0900

    [Example]react만_사용시_propsDrilling

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,65 @@
 import "./App.css";
 import React, { useState } from "react";
-
-function Counter() {
+export default function App() {
+  const [number, setNumber] = useState(1);
+  const onIncrease = () => {
+    console.log("나 호출 잘되요.");
+    setNumber(number + 1);
+  };
   return (
-    <div>
-      <button>+</button>0
+    <div className="container">
+      <h1>Root:{number}</h1>
+      <div id="grid">
+        <Left1 number={number}></Left1>
+        <Right1 onIncrease={onIncrease} number={number}></Right1>
+      </div>
     </div>
   );
 }
-export default function App() {
+function Left1(props) {
   return (
     <div>
-      <Counter />
+      <h1>Left1 : {props.number}</h1>
+      <Left2 number={props.number}></Left2>
+    </div>
+  );
+}
+function Left2(props) {
+  return (
+    <div>
+      <h1>Left2: {props.number}</h1>
+      <Left3 number={props.number}></Left3>
+    </div>
+  );
+}
+function Left3(props) {
+  return (
+    <div>
+      <h1>Left3:{props.number}</h1>
+    </div>
+  );
+}
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1:{props.number}</h1>
+      <Right2 onIncrease={props.onIncrease} number={props.number}></Right2>
+    </div>
+  );
+}
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2:{props.number}</h1>
+      <Right3 onIncrease={props.onIncrease} number={props.number}></Right3>
+    </div>
+  );
+}
+function Right3(props) {
+  return (
+    <div>
+      <h1>Right3:{props.number}</h1>
+      <input type="button" value="+" onClick={() => props.onIncrease()}></input>
     </div>
   );
 }
