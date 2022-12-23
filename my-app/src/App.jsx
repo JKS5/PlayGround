103c5b93cd85d7af44f80cd1bbda293651e5cc93
JKS5 <jokekhanson@gmail.com>
2022-12-23 20:39:00 +0900

    [Example]react-redux사용예시

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
-export default function App() {
+function Counter() {
   return (
-    <div className="conatiner">
-      <h1>Root:{}</h1>
+    <div>
+      <button>+</button>0
     </div>
   );
 }
-function Left1(props) {
+export default function App() {
   return (
     <div>
-      <h1>Left1</h1>
+      <Counter />
     </div>
   );
 }
