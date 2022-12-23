f9ce9ab5fbd39444553cd57509084e69f09fbc2d
JKS5 <jokekhanson@gmail.com>
2022-12-23 14:50:16 +0900

    [feat]Start React 4 PlayGround

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="conatiner">
+      <h1>Root:{}</h1>
+    </div>
+  );
+}
+function Left1(props) {
+  return (
+    <div>
+      <h1>Left1</h1>
+    </div>
+  );
+}
